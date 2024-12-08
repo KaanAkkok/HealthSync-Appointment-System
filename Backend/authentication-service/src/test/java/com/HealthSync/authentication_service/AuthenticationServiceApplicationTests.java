b4d93ec920745c455ac44d737f69a23a55a83bc3
Kaan Akkök <kaanakkok@gmail.com>
2024-12-08 22:57:32 +0300

    Project folder created.

---
+++ Backend/authentication-service/src/test/java/com/HealthSync/authentication_service/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HealthSync.authentication_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
